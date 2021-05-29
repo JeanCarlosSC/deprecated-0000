090d8772c9a847e0c13a46d7d5347e298c5b1e1b
JeanCarlosSC <jeancarlosodi@gmail.com>
2021-05-29 08:12:38 -0500

    updated references

--- app/src/main/java/com/jeank/soldarx/ui/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/jeank/soldarx/ui/calculator/CalculatorFragment.kt
@@ -1,4 +1,4 @@
-package com.jeank.soldarx.ui.home
+package com.jeank.soldarx.ui.calculator
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.jeank.soldarx.R
-import com.jeank.soldarx.databinding.FragmentHomeBinding
+import com.jeank.soldarx.databinding.FragmentCalculatorBinding
 
-class HomeFragment : Fragment() {
+class CalculatorFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private lateinit var calculatorViewModel: CalculatorViewModel
+    private var _binding: FragmentCalculatorBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +24,14 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        calculatorViewModel =
+            ViewModelProvider(this).get(CalculatorViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentCalculatorBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        calculatorViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/jeank/soldarx/ui/inflows/InflowsViewModel.kt
+++ app/src/main/java/com/jeank/soldarx/ui/inflows/InflowsViewModel.kt
@@ -1,13 +1,13 @@
-package com.jeank.soldarx.ui.slideshow
+package com.jeank.soldarx.ui.inflows
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class InflowsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is gallery Fragment"
     }
     val text: LiveData<String> = _text
 }
